0d4d8d267d4c761d9eac4c947c5fe964d216195e
Alex <73742087+Alexofthefoot@users.noreply.github.com>
2025-02-23 20:26:17 -0800

    starting the tictactoe project

--- Javascript refresher/Library project/book.js
+++ Javascript refresher/Library project/book.js
@@ -1,21 +1,20 @@
 const myLibrary = [];
-// var NUM_BOOKS = 0;
-var CURRENT_ID = 0;
+let CURRENT_ID = 0;
 
-function Book(title, author, numPages, isRead) {
+function Book(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
-    this.numPages = numPages;
+    this.pages = pages;
     this.isRead = isRead;
     this.identifier = CURRENT_ID;
     CURRENT_ID++;
     this.info = function() {
-        var info = title + " by " + author + ", " + numPages + ", ";
+        let info = `${this.title}, written by ${author} has ${this.pages} pages and `;
         if (this.isRead) {
-            info += "have read.";
+            info += "has been read.";
         }
         else {
-            info += "not read yet.";
+            info += "has not been read.";
         }
 
         return info;
@@ -26,23 +25,18 @@ function Book(title, author, numPages, isRead) {
 }
 
 function updatePage(book) {
-    var container = document.getElementById('library-container');
+    const container = document.getElementById('library-container');
     //The div container
-    var newDiv = document.createElement("div");
+    let newDiv = document.createElement("div");
     newDiv.className = "card";
     container.appendChild(newDiv);
     //text element
-    var newP = document.createElement("p") 
-    newP.innerHTML = `${book.title} written by ${book.author} has ${book.numPages} pages `
-    if (book.isRead) {
-        newP.innerHTML += `and has been read.`
-    }
-    else {
-        newP.innerHTML += 'and has not been read.'
-    }
+    let newP = document.createElement("p") 
+    newP.innerHTML = book.info()
+   
     newDiv.appendChild(newP);
     //toggle button
-    var button = document.createElement("button")
+    let button = document.createElement("button")
     button.innerHTML = "Toggle Read Status"
     button.type = "button"
     button.id = book.identifier
@@ -61,7 +55,6 @@ function displayForm() {
 }
 
 function changeReadStatus(button) {
-    console.log("alter book with id=" + button.id)
     // change value in library
     var idx = myLibrary.findIndex((book) => book.identifier == button.id)
     var book = myLibrary[idx]
@@ -69,13 +62,7 @@ function changeReadStatus(button) {
     // change what <p> displays 
     var parent = button.parentNode
     var p = parent.firstElementChild
-    p.innerHTML = `${book.title} written by ${book.author} has ${book.numPages} pages `
-    if (book.isRead) {
-        p.innerHTML += `and has been read.`
-    }
-    else {
-        p.innerHTML += 'and has not been read.'
-    }
+    p.innerHTML = book.info()
     
 }
 
@@ -91,24 +78,20 @@ document.getElementById("new-book").addEventListener("submit", submitForm);
 
     async function submitForm(event) {
       event.preventDefault();
-      addBookToLibrary(document.getElementById("title").value, document.getElementById("author").value, document.getElementById("pages").value, document.getElementById("read").value)
+      let book = new Book(document.getElementById("title").value, document.getElementById("author").value, document.getElementById("pages").value, document.getElementById("read").value)
+      myLibrary.push(book)
+      updatePage(book)
       document.getElementById("new-book").reset();
     }
 
 
-function addBookToLibrary(title, author, numPages, isRead) {
-    var book = new Book(title, author, numPages, isRead)
-    console.log("my id is" + book.identifier)
-    myLibrary.push(book)
-    updatePage(book)
-  }
   
 function initialSetup() {
-    var book1 = new Book("The Hobbit", "J.R.R. Tolkien", 310, true)
-    var book2 = new Book("Dune","Frank Herbert",412,false)
-    var book3 = new Book("Pride and Prejudice", "Jane Austen", 279, false)
-    var book4 = new Book("The Catcher in the Rye", "J.D.Salinger", 277, false)
-    var book5 = new Book("1984", "George Orwell", 328, true)
+    let book1 = new Book("The Hobbit", "J.R.R. Tolkien", 310, true)
+    let book2 = new Book("Dune","Frank Herbert",412,false)
+    let book3 = new Book("Pride and Prejudice", "Jane Austen", 279, false)
+    let book4 = new Book("The Catcher in the Rye", "J.D.Salinger", 277, false)
+    let book5 = new Book("1984", "George Orwell", 328, true)
     myLibrary.push(book1, book2, book3, book4, book5)
     updatePage(book1)
     updatePage(book2)
---
+++ Javascript refresher/Tic tac toe project/game.js
@@ -0,0 +1,6 @@
+// "Each little piece of functionality should be able to fit in the game, player or gameboard objects."
+
+// conventionally X plays first, so start condition could be choosing a gmae marker
+// win condition: a row on 3 in any direction
+// lose condition: computer has a row of 3 in any direction OR no more spaces to play (will avoid no more *winnable* spaces for now, and only look for a full board)
+
