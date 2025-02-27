5685995cb952fe2caba4a5e7a3fa6e4ae0bf7a8a
Alex <73742087+Alexofthefoot@users.noreply.github.com>
2025-02-26 23:45:43 -0800

    computer plays its turn

--- Javascript refresher/Tic tac toe project/game.js
+++ Javascript refresher/Tic tac toe project/game.js
@@ -1,31 +1,67 @@
 // "Each little piece of functionality should be able to fit in the game, player or gameboard objects."
 
 const GAME_OBJECT = (function () {
-    let isFinished = false;
-
-    return { isFinished }
-}) ();
+    let computerSymbol;
+    let isPlayersTurn = true;
+    let board = ["", "", "", "", "", "", "", "", ""];
+    const getBoard = () => board;
+    const getSymbol = () => computerSymbol;
+    const setSymbol = (symbol) => { computerSymbol = symbol }
+    const alterBoard = (pos, symbol) => { board[pos] = symbol; }
+    const takeTurn = () => {
+        let turn = strategize();
+        alterBoard(turn, computerSymbol)
+        UIController.computerTurn(turn, computerSymbol);
+        console.log(board)
+        if (isGameFinished()) {
+            //reset board
+            board = ["", "", "", "", "", "", "", "", ""];
+            // reset css
+            UIController.finishGame(); 
+        }
+    }
+    const strategize = () => {
+        // TODO: put in an actual strategy
+        for (let i = 0; i < board.length; i++) {
+            if (board[i] == "") {
+                console.log("computer plays position", i);
+                return i; 
+            }
+        }
+    }
+    let isGameFinished = () => {
+        // TODO put in more than one win condition lol
+        if (board[0] == board[1] && board[1] == board[2] && board[2] != ""){
+            console.log(board[0], board[1], board[2])
+            console.log("returning true")
+            return true;
+        }
+        console.log("returning false")
+        return false;
+    }
+    return { isGameFinished, isPlayersTurn, getBoard, getSymbol, setSymbol, alterBoard, takeTurn }
+})();
 
 const PLAYER = (function () {
     let playerName;
     let playerSymbol;
-    const getName = () => {return playerName}
-    const setName = (name) => {playerName = name}
-    const getSymbol = () => {return playerSymbol}
-    const setSymbol = (symbol) => {playerSymbol = symbol}
+    const getName = () => playerName;
+    const setName = (name) => { playerName = name }
+    const getSymbol = () => playerSymbol;
+    const setSymbol = (symbol) => { playerSymbol = symbol }
 
-    return {getName, setName, getSymbol, setSymbol}
+    return { getName, setName, getSymbol, setSymbol }
 })();
 
 const UIController = (function () {
     const initButtons = () => {
-                let buttons = document.querySelectorAll('button');
-                buttons.forEach( button => {
-                    if (button.id != 'submit_button') { //ignore the form submission button
-                        button.addEventListener('click', buttonPress);
-                    }
-                
-                })}
+        let buttons = document.querySelectorAll('button');
+        buttons.forEach(button => {
+            if (button.id != 'submit_button') { //ignore the form submission button
+                button.addEventListener('click', buttonPress, button.id);
+            }
+        })
+    }
 
     const initForm = () => {
         let form = document.getElementById('player_info');
@@ -40,12 +76,16 @@ const UIController = (function () {
         // verify user inputs
         if ((symbolX || symbolO) && name != null) {
             PLAYER.setName(name.value);
-            console.log("O is ", symbolO,"X is", symbolX)
+            console.log("O is ", symbolO, "X is", symbolX)
             if (symbolX) {
                 PLAYER.setSymbol('X');
+                GAME_OBJECT.setSymbol('O');
             }
-            else {PLAYER.setSymbol('O');}
-            console.log("Player is now named ", PLAYER.getName() , "with a symbol of ", PLAYER.getSymbol())
+            else {
+                PLAYER.setSymbol('O');
+                GAME_OBJECT.setSymbol('X');
+            }
+            console.log("Player is now named ", PLAYER.getName(), "with a symbol of ", PLAYER.getSymbol())
             hideForm();
             revealBoard();
         }
@@ -58,17 +98,25 @@ const UIController = (function () {
 
     const buttonPress = (event) => {    //private, but accessible by the buttons themselves?
         if (event.target.innerHTML == '') {
-                event.target.innerHTML = PLAYER.getSymbol();
-                console.log("Button clicked:", event.target);
-            }
-            else {
-                console.log("you cant do that!");
-            }} 
+            event.target.innerHTML = PLAYER.getSymbol();
+            GAME_OBJECT.alterBoard(event.target.id, PLAYER.getSymbol())
+            // tell computer to take turn
+            GAME_OBJECT.takeTurn();
+        }
+        else {
+            console.log("you cant do that!");
+        }
+    }
+
+    const computerTurn = (pos) => {
+        let button = document.getElementById(pos);
+        button.innerHTML = GAME_OBJECT.getSymbol();
+    }
 
     const hideForm = () => {
         const formContainer = document.getElementById('form_container');
         formContainer.style.display = 'none';
-        
+
     }
     const revealBoard = () => {
         const board = document.getElementById('game_container');
@@ -76,29 +124,18 @@ const UIController = (function () {
         const h3 = document.getElementById('user_instruction');
         h3.innerHTML = "Click a button to start";
     }
+    const finishGame = () => {
+        const header = document.getElementById('user_instruction');
+        header.innerHTML = "Game over! Play again?"
+        const formContainer = document.getElementById('form_container');
+        formContainer.style.display = 'flex;';
+        const board = document.getElementById('game_container');
+        board.style.display = 'none';
+        
+    }
 
     initButtons()
     initForm()
-    return { buttonPress }
+    return { buttonPress, finishGame, computerTurn }
 })();
 
-
-
-
-// function createGameBoard() {
-//     let board = ["","","","","","","","",""];
-//     const getBoard = () => board;
-// }
-
-// function createPlayer(name, symbol) {
-//     const getSymbol = () => symbol;
-//     const getName = () => name; 
-//     const greeting = () => 'hello, my name is' + name;
-//     return { getSymbol, getName, greeting };
-// }
-
-
-// const p1 = createPlayer('Alex', 'x');
-// p1.greeting();  
-// let p2 = createPlayer('Brett', 'o');
-// console.log(p1.getName() ,"plays with an ", p1.getSymbol())
